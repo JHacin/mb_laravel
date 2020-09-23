e4c21eb17e5607c7c8692b5391c70db2dee97ac5
JHacin <jan.hacin@gmail.com>
2020-09-24 00:08:51 +0200

    Add remember me / forgot PW links to login.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -45,8 +45,26 @@
 
                 <div class="field">
                     <div class="control">
+                        <label class="checkbox">
+                            <input
+                                type="checkbox"
+                                name="remember"
+                                id="remember"
+                                {{ old('remember') ? 'checked' : '' }}
+                            >
+                            Zapomni si me
+                        </label>
+                    </div>
+                </div>
+
+                <div class="field">
+                    <div class="control">
                         <button type="submit" class="button is-link">Prijava</button>
                     </div>
+                </div>
+
+                <div class="field">
+                    <a href="{{ route('password.request') }}">Pozabil/-a sem geslo</a>
                 </div>
             </form>
         </div>
