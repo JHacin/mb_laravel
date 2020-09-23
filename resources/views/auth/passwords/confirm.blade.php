72d61758632aeffded6a0bc38e0641a87d71293d
JHacin <jan.hacin@gmail.com>
2020-09-24 00:55:55 +0200

    Style the confirm password page.

--- resources/views/auth/passwords/confirm.blade.php
+++ resources/views/auth/passwords/confirm.blade.php
@@ -1,49 +1,37 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Confirm Password') }}</div>
+    <section class="section">
+        <div class="container">
+            <div>Pred nadaljevanjem prosimo, da potrdite svoje geslo.</div>
 
-                <div class="card-body">
-                    {{ __('Please confirm your password before continuing.') }}
+            <form method="POST" action="{{ route('password.confirm') }}">
+                @csrf
 
-                    <form method="POST" action="{{ route('password.confirm') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Confirm Password') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
+                <div class="field">
+                    <label class="label is-sr-only" for="password">Geslo</label>
+                    <div class="control has-icons-left">
+                        <input
+                            class="input"
+                            type="password"
+                            id="password"
+                            name="password"
+                            placeholder="Geslo"
+                            required
+                            autocomplete="current-password"
+                        >
+                        <span class="icon is-small is-left">
+                          <i class="fas fa-key"></i>
+                        </span>
+                    </div>
                 </div>
-            </div>
+
+                <div class="field">
+                    <div class="control">
+                        <button type="submit" class="button is-link">Potrdi</button>
+                    </div>
+                </div>
+            </form>
         </div>
-    </div>
-</div>
+    </section>
 @endsection
