3f9e65c38aec366f5326aa6a35c3859eb18f03c0
JHacin <jan.hacin@gmail.com>
2020-09-23 21:29:11 +0200

    Setup SCSS, Bulma & some basic Mix stuff.

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="sl">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Laravel</title>
+        <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
+    </head>
+    <body>
+        <footer class="footer has-text-centered">
+            Zavod Mačja hiša © {{ date('Y') }} Mačji boter |
+            Vse pravice pridržane |
+            Zasebnost |
+            Oblikovanje: Lana, izvedba: Jan Hacin
+        </footer>
+    </body>
+</html>
