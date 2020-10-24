f79908ae0035b9f01e2799301c3cedc123b6b320
JHacin <jan.hacin@gmail.com>
2020-10-24 21:36:22 +0200

    Group admin sidebar items.

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -1,8 +1,28 @@
-<!-- This file is used to store sidebar items, starting with Backpack\Base 0.9.0 -->
-<li class="nav-item">
+<li class='nav-item'>
     <a class="nav-link" href="{{ backpack_url(config('routes.admin.dashboard')) }}">
         <i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}
     </a>
+</li>
+
+<li class='nav-item'>
+    <div class="nav-title">Muce</div>
+    <a class='nav-link' href='{{ backpack_url(config('routes.admin.cats')) }}'>
+        <i class='nav-icon la la-cat'></i> Muce
+    </a>
+    <a class='nav-link' href='{{ backpack_url(config('routes.admin.cat_locations')) }}'>
+        <i class='nav-icon la la-map-marker'></i> Lokacije
+    </a>
+</li>
+
+<li class='nav-item'>
+    <div class="nav-title">Botrovanja</div>
+    <a class='nav-link' href='{{ backpack_url(config('routes.admin.sponsorships')) }}'>
+        <i class='nav-icon la la-hands-helping'></i> Botrovanja
+    </a>
+</li>
+
+<li class="nav-item">
+    <div class="nav-title">Uporabniki</div>
     <a class="nav-link" href="{{ backpack_url(config('routes.admin.users')) }}">
         <i class="la la-user nav-icon"></i> {{ trans('backpack::permissionmanager.users') }}
     </a>
@@ -14,20 +34,3 @@
     </a>
 </li>
 
-<li class='nav-item'>
-    <a class='nav-link' href='{{ backpack_url(config('routes.admin.cats')) }}'>
-        <i class='nav-icon la la-cat'></i> Muce
-    </a>
-</li>
-
-<li class='nav-item'>
-    <a class='nav-link' href='{{ backpack_url(config('routes.admin.sponsorships')) }}'>
-        <i class='nav-icon la la-hands-helping'></i> Botrovanja
-    </a>
-</li>
-
-<li class='nav-item'>
-    <a class='nav-link' href='{{ backpack_url(config('routes.admin.cat_locations')) }}'>
-        <i class='nav-icon la la-map-marker'></i> Lokacije
-    </a>
-</li>
