36925bf5fc00ab2a661869c6ff569d15e9fd8253
JHacin <jan.hacin@gmail.com>
2021-01-08 00:13:54 +0100

    FAQ styling.

--- resources/views/faq.blade.php
+++ resources/views/faq.blade.php
@@ -5,73 +5,93 @@
         <div class="container">
             <h1 class="title">Pravila in pogosta vprašanja</h1>
 
-            <div class="content">
-                <p class="has-text-primary has-text-weight-bold is-size-5">Kako postanem mačji boter?</p>
-                <p>Preprosto.</p>
-                <p>
-                    Oglejte si muce, ki iščejo botra. Izberite muco in ob njenem imenu kliknite
-                    <em>Želiš postati moj boter</em>? Izpolnite in pošljite Dogovor o posvojitvi na daljavo.
-                    Na svoj elektronski naslov boste prejeli sporočilo o sklenjenem botrstvu.
-                    Potem pa vsak mesec sproti nakazujete izbrani znesek.
-                </p>
-                <p>
-                    Po prejemu vaših podatkov bomo smatrali, da se vaših nakazil lahko nadejamo do
-                    prekinitve dogovora z vaše strani.
-                </p>
-            </div>
+            <article class="message is-primary">
+                <div class="message-header">Kako postanem mačji boter?</div>
+                <div class="message-body">
+                    <div class="content">
+                        <p>Preprosto.</p>
+                        <p>
+                            Oglejte si muce, ki iščejo botra. Izberite muco in ob njenem imenu kliknite
+                            <em>Želiš postati moj boter</em>? Izpolnite in pošljite <strong>Dogovor o posvojitvi na daljavo</strong>.
+                            Na svoj elektronski naslov boste prejeli sporočilo o sklenjenem botrstvu.
+                            Potem pa vsak mesec sproti nakazujete izbrani znesek.
+                        </p>
+                        <p>
+                            Po prejemu vaših podatkov bomo smatrali, da se vaših nakazil lahko nadejamo do
+                            prekinitve dogovora z vaše strani.
+                        </p>
+                    </div>
+                </div>
+            </article>
 
-            <div class="content">
-                <p class="has-text-primary has-text-weight-bold is-size-5">Ne morem se odločiti za eno muco. Kaj storiti?</p>
-                <p>
-                    Med Mucami, ki iščejo botra je tudi naš Čombe, ki je pokrovitelj vseh naših muckov.
-                    Če se ne morete odločiti za enega muca, postanite njegov boter. Čombe vam obljublja,
-                    da bo vaš denar pošteno razdelil tistim, ki ga v danem trenutku najbolj potrebujejo.
-                </p>
-                <p>
-                    Posvojite pa lahko tudi Bubije, s čemer nam pomagate pri oskrbi mačjih dudarjev in mladičev
-                    ali pa Pozitivčke, FeLV (mačja levkoza) in/ali FIV (mačji aids) pozitivne muce brez kliničnih
-                    znakov bolezni.
-                </p>
-            </div>
+            <article class="message is-primary">
+                <div class="message-header">Ne morem se odločiti za eno muco. Kaj storiti?</div>
+                <div class="message-body">
+                    <div class="content">
+                        <p>
+                            Med mucami, ki iščejo botra je tudi naš <strong>Čombe</strong>, ki je pokrovitelj vseh naših muckov.
+                            Če se ne morete odločiti za enega muca, postanite njegov boter. Čombe vam obljublja,
+                            da bo vaš denar pošteno razdelil tistim, ki ga v danem trenutku najbolj potrebujejo.
+                        </p>
+                        <p>
+                            Posvojite pa lahko tudi <strong>Bubije</strong>, s čemer nam pomagate pri oskrbi mačjih dudarjev in mladičev
+                            ali pa <strong>Pozitivčke</strong>, FeLV (mačja levkoza) in/ali FIV (mačji aids) pozitivne muce brez kliničnih
+                            znakov bolezni.
+                        </p>
+                    </div>
+                </div>
+            </article>
 
-            <div class="content">
-                <p class="has-text-primary has-text-weight-bold is-size-5">Kako prekiniti dogovor o posvojitvi na daljavo?</p>
-                <p>
-                    Če boste želeli prekiniti dogovor, nam z elektronskega naslova, ki ste ga navedli ob izpolnitvi
-                    Dogovora o posvojitvi na daljavo, pošljite elektronsko sporočilo, v katerem navedite,
-                    da odstopate od dogovora.
-                </p>
-                <p>
-                    V primeru, da vaših prispevkov za botrovanje ne bomo prejemali, si pridržujemo pravico,
-                    da vaše botrovanje prekinemo brez opozorila.
-                </p>
-            </div>
+            <article class="message is-primary">
+                <div class="message-header">Kako prekiniti dogovor o posvojitvi na daljavo?</div>
+                <div class="message-body">
+                    <div class="content">
+                        <p>
+                            Če boste želeli prekiniti dogovor, nam z elektronskega naslova, ki ste ga navedli ob izpolnitvi
+                            Dogovora o posvojitvi na daljavo, pošljite elektronsko sporočilo, v katerem navedite,
+                            da odstopate od dogovora.
+                        </p>
+                        <p>
+                            V primeru, da vaših prispevkov za botrovanje ne bomo prejemali, si pridržujemo pravico,
+                            da vaše botrovanje prekinemo brez opozorila.
+                        </p>
+                    </div>
+                </div>
+            </article>
 
-            <div class="content">
-                <p class="has-text-primary has-text-weight-bold is-size-5">Kaj dobi boter v zameno za svojo donacijo?</p>
-                <p>Botrom se bodo njihovi mucki ob različnih priložnostih oglasili z novicami.</p>
-                <p>
-                    Ime in kraj bivanja botra bo naveden ob imenu muca na spletni strani
-                    (razen v primeru, ko želi boter ostati anonimen).
-                </p>
-                <p>
-                    V primeru, da se bo za posvojitev na daljavo odločilo podjetje,
-                    lahko objavimo tudi povezavo na vašo spletno stran.
-                </p>
-                <p>
-                    Botri lahko postanete tudi član kluba Super Čombe. Članstvo v klubu vam ne prinaša nobene obveznosti,
-                    imate pa kot član ugodne pogoje pri nakupih v naši spletni trgovini Super Čombe.
-                    Za ureditev članstva nam po registraciji v spletni trgovini pišite na trgovina@supercombe.si.
-                </p>
-            </div>
+            <article class="message is-primary">
+                <div class="message-header">Kaj dobi boter v zameno za svojo donacijo?</div>
+                <div class="message-body">
+                    <div class="content">
+                        <p>Botrom se bodo njihovi mucki ob različnih priložnostih oglasili z novicami.</p>
+                        <p>
+                            Ime in kraj bivanja botra bo naveden ob imenu muca na spletni strani
+                            (razen v primeru, ko želi boter ostati anonimen).
+                        </p>
+                        <p>
+                            V primeru, da se bo za posvojitev na daljavo odločilo podjetje,
+                            lahko objavimo tudi povezavo na vašo spletno stran.
+                        </p>
+                        <p>
+                            Botri lahko postanete tudi član <strong>kluba Super Čombe</strong>. Članstvo v klubu vam ne prinaša nobene obveznosti,
+                            imate pa kot član ugodne pogoje pri nakupih v naši spletni trgovini Super Čombe.
+                            Za ureditev članstva nam po registraciji v spletni trgovini pišite na <a href="mailto:trgovina@supercombe.si">trgovina@supercombe.si</a>.
+                        </p>
+                    </div>
+                </div>
+            </article>
 
-            <div class="content">
-                <p class="has-text-primary has-text-weight-bold is-size-5">Za kaj bo porabljen moj denar?</p>
-                <p>
-                    Denar bo porabljen za oskrbo (kvalitetna hrana, pesek, sredstva proti zajedalcem,
-                    veterinarski stroški...) vaše in drugih muc pod okriljem Mačje hiše.
-                </p>
-            </div>
+            <article class="message is-primary">
+                <div class="message-header">Za kaj bo porabljen moj denar?</div>
+                <div class="message-body">
+                   <div class="content">
+                       <p>
+                           Denar bo porabljen za oskrbo (kvalitetna hrana, pesek, sredstva proti zajedalcem,
+                           veterinarski stroški...) vaše in drugih muc pod okriljem Mačje hiše.
+                       </p>
+                   </div>
+                </div>
+            </article>
         </div>
     </section>
 @endsection
