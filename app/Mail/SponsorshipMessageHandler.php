f228ac4706118afcb179f53304c21b3e9b045736
JHacin <jan.hacin@gmail.com>
2021-03-24 23:56:22 +0100

    send cat/sponsor gender as vars in SponsorshipMessageHandler

--- app/Mail/SponsorshipMessageHandler.php
+++ app/Mail/SponsorshipMessageHandler.php
@@ -4,12 +4,13 @@
 
 namespace App\Mail;
 
+use App\Models\Cat;
+use App\Models\PersonData;
 use App\Models\SponsorshipMessage;
 use MailClient;
 
 class SponsorshipMessageHandler
 {
-
     /**
      * @param \App\Models\SponsorshipMessage $message
      * @noinspection PhpUnnecessaryFullyQualifiedNameInspection
@@ -22,7 +23,9 @@ class SponsorshipMessageHandler
             'subject' => $message->messageType->subject,
             'template' => $message->messageType->template_id,
             'v:ime_botra' => $message->personData->first_name,
+            'v:spol_botra' => $message->personData->gender === PersonData::GENDER_MALE ? 'M' : 'F',
             'v:ime_muce' => $message->cat->name,
+            'v:spol_muce' => $message->cat->gender === Cat::GENDER_MALE ? 'M' : 'F',
         ]);
     }
 }
--- tests/Unit/Mail/SponsorshipMessageHandlerTest.php
+++ tests/Unit/Mail/SponsorshipMessageHandlerTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Unit\Mail;
 
+use App\Models\Cat;
+use App\Models\PersonData;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use MailClient;
 use SponsorshipMessageHandler;
@@ -13,12 +15,8 @@ class SponsorshipMessageHandlerTest extends TestCase
 
     protected bool $seed = true;
 
-    /**
-     * @return void
-     */
     public function test_sends_message_with_correct_params()
     {
-
         $msg = $this->createSponsorshipMessage([
             'message_type_id' => $this->createSponsorshipMessageType()->id,
             'person_data_id' => $this->createPersonData()->id,
@@ -33,7 +31,57 @@ class SponsorshipMessageHandlerTest extends TestCase
                 'subject' => $msg->messageType->subject,
                 'template' => $msg->messageType->template_id,
                 'v:ime_botra' => $msg->personData->first_name,
+                'v:spol_botra' => $msg->personData->gender === PersonData::GENDER_MALE ? 'M' : 'F',
                 'v:ime_muce' => $msg->cat->name,
+                'v:spol_muce' => $msg->cat->gender === Cat::GENDER_MALE ? 'M' : 'F',
+            ]);
+
+        SponsorshipMessageHandler::send($msg);
+    }
+
+    public function test_passes_correct_male_gender_abbreviation()
+    {
+        $msg = $this->createSponsorshipMessage([
+            'message_type_id' => $this->createSponsorshipMessageType()->id,
+            'person_data_id' => $this->createPersonData(['gender' => PersonData::GENDER_MALE])->id,
+            'cat_id' =>  $this->createCat(['gender' => Cat::GENDER_MALE])->id,
+        ]);
+
+        MailClient::shouldReceive('send')
+            ->once()
+            ->with([
+                'to' => $msg->personData->email,
+                'bcc' => env('MAIL_BCC_COPY_ADDRESS'),
+                'subject' => $msg->messageType->subject,
+                'template' => $msg->messageType->template_id,
+                'v:ime_botra' => $msg->personData->first_name,
+                'v:spol_botra' => 'M',
+                'v:ime_muce' => $msg->cat->name,
+                'v:spol_muce' => 'M',
+            ]);
+
+        SponsorshipMessageHandler::send($msg);
+    }
+
+    public function test_passes_correct_female_gender_abbreviation()
+    {
+        $msg = $this->createSponsorshipMessage([
+            'message_type_id' => $this->createSponsorshipMessageType()->id,
+            'person_data_id' => $this->createPersonData(['gender' => PersonData::GENDER_FEMALE])->id,
+            'cat_id' =>  $this->createCat(['gender' => Cat::GENDER_FEMALE])->id,
+        ]);
+
+        MailClient::shouldReceive('send')
+            ->once()
+            ->with([
+                'to' => $msg->personData->email,
+                'bcc' => env('MAIL_BCC_COPY_ADDRESS'),
+                'subject' => $msg->messageType->subject,
+                'template' => $msg->messageType->template_id,
+                'v:ime_botra' => $msg->personData->first_name,
+                'v:spol_botra' => 'F',
+                'v:ime_muce' => $msg->cat->name,
+                'v:spol_muce' => 'F',
             ]);
 
         SponsorshipMessageHandler::send($msg);
