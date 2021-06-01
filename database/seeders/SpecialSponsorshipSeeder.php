5268ef26940e8f25001a7c7bc53e7b640fe8aa63
JHacin <jan.hacin@gmail.com>
2021-06-01 23:30:30 +0200

    sponsors of this month - empty state

--- database/seeders/SpecialSponsorshipSeeder.php
+++ database/seeders/SpecialSponsorshipSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\SpecialSponsorship;
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 
 class SpecialSponsorshipSeeder extends Seeder
@@ -10,5 +11,6 @@ class SpecialSponsorshipSeeder extends Seeder
     public function run()
     {
         SpecialSponsorship::factory()->count(50)->create();
+        SpecialSponsorship::factory(['confirmed_at' => Carbon::now()])->count(25)->create();
     }
 }
--- resources/views/components/special-sponsorships/sponsors-of-this-month.blade.php
+++ resources/views/components/special-sponsorships/sponsors-of-this-month.blade.php
@@ -1,15 +1,22 @@
 <div class="has-background-white-ter px-5 py-4">
     <h4 class="title is-4">{{ $title }}</h4>
 
-    @foreach($sponsorsPerType as $type => $sponsors)
-        <div class="{{ !$loop->last ? 'mb-4' : '' }}">
-            <h5 class="has-text-primary has-text-weight-semibold">
-                {{ \App\Models\SpecialSponsorship::TYPE_LABELS[$type] }}
-            </h5>
+    @if(count($sponsorsPerType) === 0)
+        <div>V tem mescu še nismo imeli novih botrov.</div>
+    @else
+        <div class="mb-5">
+            @foreach($sponsorsPerType as $type => $sponsors)
+                <div class="{{ !$loop->last ? 'mb-4' : '' }}">
+                    <h5 class="has-text-primary has-text-weight-semibold">
+                        {{ \App\Models\SpecialSponsorship::TYPE_LABELS[$type] }}
+                    </h5>
 
-            @foreach($sponsors as $sponsor)
-                <x-sponsor-details :sponsor="$sponsor"/>
+                    @foreach($sponsors as $sponsor)
+                        <x-sponsor-details :sponsor="$sponsor"/>
+                    @endforeach
+                </div>
             @endforeach
         </div>
-    @endforeach
+        <div><strong>Hvala vsem!</strong></div>
+    @endif
 </div>
