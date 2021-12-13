402fc6107e59bdd99dd9f1d3bab0af67651ddc29
JHacin <jan.hacin@gmail.com>
2021-12-13 02:10:28 +0100

    move front page to tailwind

--- resources/views/components/hero-cat.blade.php
+++ resources/views/components/hero-cat.blade.php
@@ -1,17 +1,19 @@
-<div class="hero-cat-column column is-6-tablet is-4-widescreen">
-    <a href="{{ route('cat_details', $cat) }}" class="is-flex-grow-1 is-flex">
-        <div class="hero-cat">
-            <img src="{{ $photo_url }}" alt="{{ $cat->name }}" class="hero-cat__image">
+<div class="tw-flex md:last:tw-hidden xl:last:tw-block">
+    <a href="{{ route('cat_details', $cat) }}" class="tw-flex tw-flex-grow">
+        <div class="tw-relative tw-flex tw-shadow tw-flex-grow tw-transition-opacity hover:tw-opacity-90">
+            <img src="{{ $photo_url }}" alt="{{ $cat->name }}" class="tw-flex-1 tw-w-full">
 
-            <div class="hero-cat-label">
-                <div class="hero-cat-label__icon">
+            <div class="tw-absolute tw-bottom-6 tw-left-6 md:tw-left-12 tw-right-6 md:tw-right-12 tw-flex
+            tw-items-center tw-flex-col md:tw-flex-row tw-text-white tw-py-3 tw-px-4
+            tw-rounded-xl tw-shadow tw-bg-primary/90">
+                <div class="tw-text-2xl md:tw-text-4xl md:tw-mr-5 tw-text-[#edc0ad]">
                     <i class="fas fa-paw"></i>
                 </div>
                 <div>
-                    <div class="hero-cat-label__name">
+                    <div class="tw-text-center md:tw-text-left tw-text-xl md:tw-text-2xl tw-font-bold">
                         {{ $cat->name }}
                     </div>
-                    <div class="hero-cat-label__duration-of-stay">
+                    <div class="tw-text-center md:tw-text-left tw-text-sm md:tw-text-base">
                         {{ $duration_of_stay }} v Mačji hiši
                     </div>
                 </div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -24,34 +24,34 @@
 @endphp
 
 @section('content')
-    <div class="columns is-variable is-1 mb-0">
+    <div class="tw-grid tw-grid-cols-1 md:tw-grid-cols-2 xl:tw-grid-cols-3 tw-gap-2">
         @foreach($heroCats as $heroCat)
             <x-hero-cat :cat="$heroCat" />
         @endforeach
     </div>
 
-    <section class="section">
-        <div class="container is-max-widescreen">
-            <div class="columns is-multiline mb-6">
+    <section class="tw-block tw-py-12 tw-px-6 md:tw-px-12">
+        <div class="tw-container tw-mx-auto tw-max-w-screen-xl">
+            <div class="tw-grid tw-grid-cols-1 md:tw-grid-cols-2 xl:tw-grid-cols-4 tw-gap-3 md:tw-gap-10 tw-mb-12">
                 @foreach($stats as $stat)
-                    <div class="stat-column column is-6-tablet is-3-widescreen">
-                        <div class="stat-column__value">
+                    <div class="tw-text-center">
+                        <div class="tw-text-secondary tw-font-bold tw-text-5xl md:tw-text-6xl tw-mb-2">
                             {{ $stat['value'] }}
                         </div>
-                        <div class="stat-column__text">
+                        <div class="tw-text-primary tw-font-semibold tw-px-[20%] md:tw-px-[15%] xl:tw-px-[7.5%]">
                             {{ $stat['text'] }}
                         </div>
                     </div>
                 @endforeach
             </div>
 
-            <div class="block">
+            <div class="tw-mb-6">
                 Mačji boter je projekt <strong>Mačje hiše</strong>, ki omogoča posvojitve muck na daljavo.
                 Namenjen je vsem tistim, ki nam želite <strong>pomagati pri oskrbi nekoč brezdomnih muck</strong>.
                 Gre za obliko donacije, ki donatorja poveže z določenim mucem ali skupino mucov in mu
                 hkrati <strong>omogoča boljši vpogled v to, za kaj je bil porabljen njegov prispevek</strong>.
             </div>
-            <div class="block">
+            <div class="tw-mb-6">
                 V projekt Mačji boter so vključeni predvsem tisti mucki, ki iz različnih vzrokov
                 <strong>dalj časa iščejo svoj dom</strong> ali pa zaradi njihovih posebnosti
                 predvidevamo, da bodo pri nas dalj časa. S pomočjo mačjih botrov jim v skrbništvu
@@ -59,7 +59,7 @@
                 kar potrebujejo za srečno mačje življenje.
                 Tudi skrb, ljubezen, neprespane noči, solze, ko je hudo, in radost, ko gre na bolje.
             </div>
-            <div class="block">
+            <div class="tw-mb-6">
                 Mucam v oskrbi Mačje hiše lahko v okviru botrstva pomagate na več načinov.
                 Postanete lahko
                 <a href="{{ route('why_become_sponsor') }}" dusk="home-why-become-sponsor-link"><strong>redni boter</strong></a>,
@@ -69,7 +69,7 @@
                 <a href="{{ route('special_sponsorships') }}" dusk="home-special-sponsorships-link"><strong>posebnih vrst botrstva</strong></a>.
                 Vse vrste pomoči so več kot dobrodošle in potrebne.
             </div>
-            <div class="block">
+            <div class="tw-mb-6">
                 Botrstvo lahko tudi
                 <a href="{{ route('gift_sponsorship') }}" dusk="home-gift-sponsorship-link"><strong>podarite</strong></a>
                 in z njim razveselite obdarovanca
@@ -77,7 +77,7 @@
                 ponujamo več različnih možnosti in upamo,
                 da boste med njimi našli tisto pravo ustrezno darilo, ki je eno, a osreči mnoge.
             </div>
-            <div class="block">
+            <div class="tw-mb-6">
                 <strong>Hvala za vso pomoč, muce jo resnično potrebujejo!</strong>
             </div>
         </div>
