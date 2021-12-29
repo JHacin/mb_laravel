4f59dcfb9512cd69379829ea347ee5fff32110ea
JHacin <jan.hacin@gmail.com>
2021-12-29 22:11:16 +0100

    migrate nav first draft to tailwind

--- resources/views/components/layout/navbar.blade.php
+++ resources/views/components/layout/navbar.blade.php
@@ -22,38 +22,64 @@
             'route_name' => 'news',
             'dusk' => 'navbar-news-link',
         ],
-    ]
+    ];
+
+    $socialLinks = [
+        [
+            'href' => 'mailto:' . config('links.contact_email'),
+            'icon' => 'fas fa-envelope',
+            'dusk' => 'navbar-contact-email-link',
+        ],
+        [
+            'href' => config('links.instagram_page'),
+            'icon' => 'fab fa-instagram-square',
+            'dusk' => 'navbar-instagram-link',
+        ],
+        [
+            'href' => config('links.facebook_page'),
+            'icon' => 'fab fa-facebook',
+            'dusk' => 'navbar-facebook-link',
+        ],
+    ];
 @endphp
 
-
-<nav
-    class="navbar is-primary"
-    role="navigation"
-    aria-label="glavni meni"
->
-    <div class="navbar-brand">
-        <a class="navbar-item" href="{{ route('home') }}" dusk="navbar-home-link">
-            <img class="nav-logo" src="{{ mix('img/logo.png') }}" alt="Mačji boter">
-        </a>
-
+<header class="tw-bg-primary">
+    <nav
+        role="navigation"
+        aria-label="glavni meni"
+        class="
+        tw-px-4 tw-py-3 tw-grid tw-grid-cols-[60px_1fr_auto] tw-items-center
+        xl:tw-grid-cols-[132px_1fr] xl:tw-grid-rows-[1fr_1fr] tw-max-w-screen-xl tw-mx-auto
+    "
+    >
         <a
-            role="button"
-            class="navbar-burger burger"
-            aria-label="meni"
-            aria-expanded="false"
-            data-target="navbar"
+            href="{{ route('home') }}"
+            dusk="navbar-home-link"
+            class="xl:tw-row-span-2"
         >
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
+            <img src="{{ mix('img/logo.png') }}" alt="Mačji boter">
         </a>
-    </div>
 
-    <div id="navbar" class="navbar-menu">
-        <div class="navbar-start is-justify-content-center is-flex-grow-1">
+        <div class="tw-flex tw-justify-self-end tw-mr-4 xl:tw-mr-0">
+            @foreach($socialLinks as $socialLink)
+                <a
+                    href="{{ $socialLink['href'] }}"
+                    class="tw-text-white hover:tw-text-gray-200 tw-mx-1 tw-text-xl"
+                    dusk="{{ $socialLink['dusk'] }}"
+                    target="_blank"
+                >
+                    <i class="{{ $socialLink['icon'] }}"></i>
+                </a>
+            @endforeach
+        </div>
+
+        <div class="tw-hidden xl:tw-flex tw-justify-end">
             @foreach($pageLinks as $pageLink)
                 <a
-                    class="navbar-item nav-link{{ $currentRouteName === $pageLink['route_name'] ? ' nav-link--active' : '' }}"
+                    class="
+                        tw-text-white tw-font-semibold tw-p-3 hover:tw-bg-white
+                        {{ $currentRouteName === $pageLink['route_name'] ? ' tw-bg-white tw-text-black' : '' }}
+                    "
                     href="{{ route($pageLink['route_name']) }}"
                     dusk="{{ $pageLink['dusk'] }}"
                 >
@@ -61,48 +87,18 @@
                 </a>
             @endforeach
         </div>
-        <div class="navbar-end">
-            <div class="navbar-item">
-                @include('components.layout.navbar-social-links', ['link_class' => 'nav-social-link--navbar'])
-            </div>
-        </div>
-    </div>
-</nav>
 
-{{--                <div class="navbar-end">--}}
-{{--                    <div class="navbar-item has-dropdown is-hoverable" dusk="navbar-profile-section">--}}
-{{--                        <a class="navbar-link is-arrowless px-4">--}}
-{{--                            <span class="icon is-medium">--}}
-{{--                                <i class="fas fa-user is-size-5"></i>--}}
-{{--                            </span>--}}
-{{--                        </a>--}}
-
-{{--                        <div class="navbar-dropdown is-right">--}}
-{{--                            @auth--}}
-{{--                                <a--}}
-{{--                                    class="navbar-item"--}}
-{{--                                    href="{{ route('user-profile') }}"--}}
-{{--                                    dusk="nav-profile-button"--}}
-{{--                                >--}}
-{{--                                    Profil--}}
-{{--                                </a>--}}
-
-{{--                                <form id="logout-form" action="{{ route('logout') }}" method="POST">--}}
-{{--                                    @csrf--}}
-{{--                                    <button type="submit" class="navbar-item button" dusk="nav-logout-button">--}}
-{{--                                        Odjava--}}
-{{--                                    </button>--}}
-{{--                                </form>--}}
-{{--                            @endauth--}}
-
-{{--                            @guest--}}
-{{--                                <a class="navbar-item" href="{{ route('register') }}" dusk="nav-register-button">--}}
-{{--                                    Registracija--}}
-{{--                                </a>--}}
-{{--                                <a class="navbar-item" href="{{ route('login') }}" dusk="nav-login-button">--}}
-{{--                                    Prijava--}}
-{{--                                </a>--}}
-{{--                            @endguest--}}
-{{--                        </div>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
+        <a
+            role="button"
+            class="
+            xl:tw-hidden
+            tw-flex tw-items-center tw-w-[40px] tw-h-[26px] tw-cursor-pointer
+            tw-border-y-2 tw-border-white hover:tw-border-gray-200 hover:before:tw-border-gray-200
+            before:tw-block before:tw-w-full before:tw-h-0 before:tw-border-white before:tw-border-b-2
+        "
+            aria-label="meni"
+            aria-expanded="false"
+        >
+        </a>
+    </nav>
+</header>
