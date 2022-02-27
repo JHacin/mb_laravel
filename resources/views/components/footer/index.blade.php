27942a03f528f3aff28d4281eab863b16ae8fb90
JHacin <jan.hacin@gmail.com>
2022-02-27 01:04:13 +0100

    use different logos on home & other pages

--- resources/views/components/footer/index.blade.php
+++ resources/views/components/footer/index.blade.php
@@ -1,97 +1,153 @@
 <footer class="bg-gray-extralight shadow-lg">
-  <div class="mb-page-content-container">
-    <div class="mb-12 lg:mb-14 lg:flex lg:justify-between lg:items-center xl:mb-16 2xl:mb-18">
-      <div class="mb-6">
-        <img
-          src="{{ mix('img/logo.svg') }}"
-          alt="Mačji boter"
-          class="h-auto w-[115px] lg:w-[135px] 2xl:w-[155px]"
-        >
-      </div>
+    <div class="mb-page-content-container">
+        <div class="mb-12 lg:mb-14 lg:flex lg:justify-between lg:items-center xl:mb-16 2xl:mb-18">
+            <div class="mb-6">
+                <img
+                    src="{{ mix('img/logo.svg') }}"
+                    alt="Mačji boter"
+                    class="h-[60px] md:h-[75px] lg:h-[90px]"
+                >
+            </div>
 
-      <div class="mb-typography-content-base mb-font-primary-bold">
-        <span class="text-primary">Hvala</span>
-        za vso pomoč, muce jo resnično potrebujejo.
-      </div>
+            <div class="mb-typography-content-base mb-font-primary-bold">
+                <span class="text-primary">Hvala</span>
+                za vso pomoč, muce jo resnično potrebujejo.
+            </div>
+        </div>
+
+        <div class="text-gray-dark grid pb-2 lg:grid-cols-3 lg:pb-6 2xl:grid-cols-4 2xl:pb-8 xl:gap-36 2xl:gap-48">
+            <div
+                class="mb-8 space-y-8 lg:space-y-0 lg:col-span-2 lg:col-start-2 lg:gap-12 lg:flex 2xl:col-span-3 xl:gap-24 2xl:gap-36">
+                <div>
+                    <h6 class="mb-typography-content-base mb-3 lg:mb-4 xl:mb-5 2xl:mb-6">Obiščite tudi</h6>
+                    <div class="text-gray-dark text-sm font-light space-y-2">
+                        <a
+                            class="block hover:underline"
+                            href="{{ config('links.macja_hisa') }}"
+                            dusk="footer-mh-link"
+                        >
+                            Mačja hiša
+                        </a>
+                        <a
+                            class="block hover:underline"
+                            href="{{ config('links.veterina_mh') }}"
+                            dusk="footer-vet-link"
+                        >
+                            Veterina MH
+                        </a>
+                        <a
+                            class="block hover:underline"
+                            href="{{ config('links.super_combe') }}"
+                            dusk="footer-combe-link"
+                        >
+                            Spletna trgovina Super Čombe
+                        </a>
+                        <a
+                            class="block hover:underline"
+                            href="{{ config('links.pomagamo_zivalim') }}"
+                            dusk="footer-pomagamo-zivalim-link"
+                        >
+                            Forumi Pomagamo živalim
+                        </a>
+                    </div>
+                </div>
+
+                <div>
+                    <h6 class="mb-typography-content-base mb-3 lg:mb-4 xl:mb-5 2xl:mb-6">Botrstvo</h6>
+                    <div class="text-sm font-light space-y-2">
+                        <a
+                            class="block hover:underline"
+                            href="{{ route('cat_list') }}"
+                        >Muce, ki iščejo dom</a>
+                        <a
+                            class="block hover:underline"
+                            href="{{ route('why_become_sponsor') }}"
+                        >Zakaj postati mačji boter</a>
+                        <a
+                            class="block hover:underline"
+                            href="{{ route('special_sponsorships') }}"
+                        >Posebna botrstva</a>
+                        <a
+                            class="block hover:underline"
+                            href="{{ route('gift_sponsorship') }}"
+                        >Podari botrstvo</a>
+                        <a
+                            class="block hover:underline"
+                            href="{{ route('special_sponsorships_archive') }}"
+                        >Arhiv botrov</a>
+                    </div>
+                </div>
+
+                <div>
+                    <h6 class="mb-typography-content-base mb-3 lg:mb-4 xl:mb-5 2xl:mb-6">O projektu</h6>
+                    <div class="text-sm font-light space-y-2">
+                        <a
+                            class="block hover:underline"
+                            href="{{ route('news') }}"
+                        >Novice</a>
+                        <a
+                            class="block hover:underline"
+                            href="{{ route('faq') }}"
+                        >Pogosta vprašanja</a>
+                    </div>
+                </div>
+
+            </div>
+
+            <div class="lg:col-start-1 lg:row-start-1 space-y-8 mb-8">
+                <div>
+                    <h6 class="mb-typography-content-base mb-3 lg:mb-4 xl:mb-5 2xl:mb-6">Spremljajte nas</h6>
+                    <div class="space-y-2">
+                        <a
+                            class="text-sm font-light flex items-center space-x-2"
+                            href="{{ config('links.facebook_page') }}"
+                        >
+                            <x-icon
+                                class="hover:text-primary w-[12px]"
+                                icon="facebook"
+                            ></x-icon>
+                            <span class="hover:underline">Facebook</span>
+                        </a>
+                        <a
+                            class="text-sm font-light flex items-center space-x-2"
+                            href="{{ config('links.instagram_page') }}"
+                        >
+                            <x-icon
+                                class="hover:text-primary w-[12px]"
+                                icon="instagram"
+                            ></x-icon>
+                            <span class="hover:underline">Instagram</span>
+                        </a>
+                    </div>
+                </div>
+
+                <div>
+                    <h6 class="mb-typography-content-base mb-1 xl:mb-2">Kontakt</h6>
+                    <div class="text-sm font-light flex items-center space-x-2">
+                        <x-icon icon="email"></x-icon>
+                        <a
+                            class="hover:underline"
+                            href="mailto:{{ config('links.contact_email') }}"
+                        >{{ config('links.contact_email') }}</a>
+                    </div>
+                </div>
+
+                <div>
+                    <div class="text-base mb-1 xl:mb-2">Želite sponzorirati projekt?</div>
+                    <div class="text-sm font-light">
+                        Pišite nam na <a
+                            class="hover:underline"
+                            href="mailto:{{ config('links.mh_email') }}"
+                        >info@macjahisa.si</a>.
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <hr class="border-gray-light">
+
+        <div class="pt-4 lg:pt-6 xl:pt-8 2xl:pt-10">
+            <x-footer.copy></x-footer.copy>
+        </div>
     </div>
-
-    <div class="text-gray-dark grid pb-2 lg:grid-cols-3 lg:pb-6 2xl:grid-cols-4 2xl:pb-8 xl:gap-36 2xl:gap-48">
-      <div class="mb-8 space-y-8 lg:space-y-0 lg:col-span-2 lg:col-start-2 lg:gap-12 lg:flex 2xl:col-span-3 xl:gap-24 2xl:gap-36">
-        <div>
-          <h6 class="mb-typography-content-base mb-3 lg:mb-4 xl:mb-5 2xl:mb-6">Obiščite tudi</h6>
-          <div class="text-gray-dark text-sm font-light space-y-2">
-            <a class="block hover:underline" href="{{ config('links.macja_hisa') }}" dusk="footer-mh-link">
-              Mačja hiša
-            </a>
-            <a class="block hover:underline" href="{{ config('links.veterina_mh') }}" dusk="footer-vet-link">
-              Veterina MH
-            </a>
-            <a class="block hover:underline" href="{{ config('links.super_combe') }}" dusk="footer-combe-link">
-              Spletna trgovina Super Čombe
-            </a>
-            <a class="block hover:underline" href="{{ config('links.pomagamo_zivalim') }}" dusk="footer-pomagamo-zivalim-link">
-              Forumi Pomagamo živalim
-            </a>
-          </div>
-        </div>
-
-        <div>
-          <h6 class="mb-typography-content-base mb-3 lg:mb-4 xl:mb-5 2xl:mb-6">Botrstvo</h6>
-          <div class="text-sm font-light space-y-2">
-            <a class="block hover:underline" href="{{ route('cat_list') }}">Muce, ki iščejo dom</a>
-            <a class="block hover:underline" href="{{ route('why_become_sponsor') }}">Zakaj postati mačji boter</a>
-            <a class="block hover:underline" href="{{ route('special_sponsorships') }}">Posebna botrstva</a>
-            <a class="block hover:underline" href="{{ route('gift_sponsorship') }}">Podari botrstvo</a>
-            <a class="block hover:underline" href="{{ route('special_sponsorships_archive') }}">Arhiv botrov</a>
-          </div>
-        </div>
-
-        <div>
-          <h6 class="mb-typography-content-base mb-3 lg:mb-4 xl:mb-5 2xl:mb-6">O projektu</h6>
-          <div class="text-sm font-light space-y-2">
-            <a class="block hover:underline" href="{{ route('news') }}">Novice</a>
-            <a class="block hover:underline" href="{{ route('faq') }}">Pogosta vprašanja</a>
-          </div>
-        </div>
-
-      </div>
-
-      <div class="lg:col-start-1 lg:row-start-1 space-y-8 mb-8">
-        <div>
-          <h6 class="mb-typography-content-base mb-3 lg:mb-4 xl:mb-5 2xl:mb-6">Spremljajte nas</h6>
-          <div class="space-y-2">
-            <a class="text-sm font-light flex items-center space-x-2" href="{{ config('links.facebook_page') }}">
-              <x-icon class="hover:text-primary w-[12px]" icon="facebook"></x-icon>
-              <span class="hover:underline">Facebook</span>
-            </a>
-            <a class="text-sm font-light flex items-center space-x-2" href="{{ config('links.instagram_page') }}">
-              <x-icon class="hover:text-primary w-[12px]" icon="instagram"></x-icon>
-              <span class="hover:underline">Instagram</span>
-            </a>
-          </div>
-        </div>
-
-        <div>
-          <h6 class="mb-typography-content-base mb-1 xl:mb-2">Kontakt</h6>
-          <div class="text-sm font-light flex items-center space-x-2">
-            <x-icon icon="email"></x-icon>
-            <a class="hover:underline" href="mailto:{{ config('links.contact_email') }}">{{ config('links.contact_email') }}</a>
-          </div>
-        </div>
-
-        <div>
-          <div class="text-base mb-1 xl:mb-2">Želite sponzorirati projekt?</div>
-          <div class="text-sm font-light">
-            Pišite nam na <a class="hover:underline" href="mailto:{{ config('links.mh_email') }}">info@macjahisa.si</a>.
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <hr class="border-gray-light">
-
-    <div class="pt-4 lg:pt-6 xl:pt-8 2xl:pt-10">
-      <x-footer.copy></x-footer.copy>
-    </div>
-  </div>
 </footer>
--- resources/views/components/layout/header.blade.php
+++ resources/views/components/layout/header.blade.php
@@ -1,52 +1,54 @@
 @php
-    $currentRouteName = Route::currentRouteName();
+$currentRouteName = Route::currentRouteName();
+$isHomepage = $currentRouteName === 'home';
 
-    $pageLinks = [
-        [
-            'label' => 'Redno botrstvo',
-            'route_name' => 'cat_list',
-            'dusk' => 'navbar-cat-list-link',
-        ],
-        [
-            'label' => 'Posebna botrstva',
-            'route_name' => 'special_sponsorships',
-            'dusk' => 'navbar-special-sponsorships-link',
-        ],
-        [
-            'label' => 'Podari botrstvo',
-            'route_name' => 'gift_sponsorship',
-            'dusk' => 'navbar-gift-sponsorship-link',
-        ],
-        [
-            'label' => 'Novice',
-            'route_name' => 'news',
-            'dusk' => 'navbar-news-link',
-        ],
-        [
-            'label' => 'Pogosta vprašanja',
-            'route_name' => 'faq',
-            'dusk' => 'navbar-faq-link',
-        ],
-    ];
+$pageLinks = [
+    [
+        'label' => 'Redno botrstvo',
+        'route_name' => 'cat_list',
+        'dusk' => 'navbar-cat-list-link',
+    ],
+    [
+        'label' => 'Posebna botrstva',
+        'route_name' => 'special_sponsorships',
+        'dusk' => 'navbar-special-sponsorships-link',
+    ],
+    [
+        'label' => 'Podari botrstvo',
+        'route_name' => 'gift_sponsorship',
+        'dusk' => 'navbar-gift-sponsorship-link',
+    ],
+    [
+        'label' => 'Novice',
+        'route_name' => 'news',
+        'dusk' => 'navbar-news-link',
+    ],
+    [
+        'label' => 'Pogosta vprašanja',
+        'route_name' => 'faq',
+        'dusk' => 'navbar-faq-link',
+    ],
+];
 @endphp
 
 {{-- Desktop nav --}}
-<header
- class="flex items-center h-[64px] md:h-[95px] xl:h-[178px] 2xl:h-[206px]"
->
+<header class="flex items-center py-2 md:py-4 lg:py-6 xl:py-8 2xl:py-10">
     <div class="mb-container flex-grow">
         <nav
             role="navigation"
             aria-label="glavni meni"
-            class=" flex justify-between space-x-4"
+            class="flex justify-between space-x-4"
         >
             <div class="flex items-center justify-between">
                 <span
-                  class="lg:hidden flex items-center cursor-pointer p-2 mr-4"
-                  role="button"
-                  data-mobile-nav-open-btn
+                    class="lg:hidden flex items-center cursor-pointer p-2 mr-4"
+                    role="button"
+                    data-mobile-nav-open-btn
                 >
-                    <x-icon icon="burger" class="text-2xl"></x-icon>
+                    <x-icon
+                        icon="burger"
+                        class="text-2xl"
+                    ></x-icon>
                 </span>
 
                 <a
@@ -54,16 +56,19 @@
                     dusk="navbar-home-link"
                 >
                     <img
-                        src="{{ mix('img/logo.svg') }}"
+                        src="{{ mix($isHomepage ? 'img/logo.svg' : 'img/logo_without_text.svg') }}"
                         alt="Mačji boter"
-                        class="h-auto w-[70px] md:w-[80px] lg:w-[100px] xl:w-[135px] 2xl:w-[165px]"
+                        @class([
+                            'h-[45px] md:h-[60px] lg:h-[75px] xl:h-[90px] 2xl:h-[110px]' => !$isHomepage,
+                            'h-[75px] md:h-[90px] lg:h-[105px] xl:h-[130px] 2xl:h-[160px]' => $isHomepage,
+                        ])
                     >
                 </a>
             </div>
 
             <div class="flex items-center">
                 <div class="hidden lg:flex items-center space-x-4 mr-6 xl:space-x-6">
-                    @foreach($pageLinks as $pageLink)
+                    @foreach ($pageLinks as $pageLink)
                         <a
                             class="
                             text-black
@@ -72,15 +77,13 @@
                             2xl:text-xl
                             transition-all
                             hover:text-gray-semi
-                            @if($currentRouteName === $pageLink['route_name'])
-                                underline
+                            @if ($currentRouteName === $pageLink['route_name']) underline
                                 underline-offset-4
                                 decoration-dashed
                                 decoration-2
                                 decoration-primary
                                 hover:decoration-primary
-                                hover:text-black
-                            @endif
+                                hover:text-black @endif
                                 "
                             href="{{ route($pageLink['route_name']) }}"
                             dusk="{{ $pageLink['dusk'] }}"
@@ -103,36 +106,37 @@
 
 {{-- Mobile nav --}}
 <div
-  class="hidden fixed top-0 left-0 w-full h-full bg-white z-50 flex overflow-hidden"
-  data-mobile-nav
+    class="hidden fixed top-0 left-0 w-full h-full bg-white z-50 flex overflow-hidden"
+    data-mobile-nav
 >
     <div class="grow flex flex-col">
         <div class="px-4 space-y-10">
             <div class="flex justify-between items-center mt-4">
-                <a
-                  href="{{ route('home') }}"
-                >
+                <a href="{{ route('home') }}">
                     <img
-                      src="{{ mix('img/logo.svg') }}"
-                      alt="Mačji boter"
-                      class="h-auto w-[70px]"
+                        src="{{ mix('img/logo.svg') }}"
+                        alt="Mačji boter"
+                        class="h-[60px]"
                     >
                 </a>
 
                 <span
-                  class="flex items-center cursor-pointer p-2"
-                  role="button"
-                  data-mobile-nav-close-btn
+                    class="flex items-center cursor-pointer p-2"
+                    role="button"
+                    data-mobile-nav-close-btn
                 >
-              <x-icon icon="close" class="text-2xl"></x-icon>
-            </span>
+                    <x-icon
+                        icon="close"
+                        class="text-2xl"
+                    ></x-icon>
+                </span>
             </div>
 
             <div>
-                @foreach($pageLinks as $pageLink)
+                @foreach ($pageLinks as $pageLink)
                     <a
-                      class="block border-b border-gray-light py-3 text-lg font-semibold"
-                      href="{{ route($pageLink['route_name']) }}"
+                        class="block border-b border-gray-light py-3 text-lg font-semibold"
+                        href="{{ route($pageLink['route_name']) }}"
                     >
                         {{ $pageLink['label'] }}
                     </a>
@@ -152,10 +156,16 @@
                 <h6 class="mb-2">Spremljajte nas</h6>
                 <div class="flex gap-6 text-xl">
                     <a href="{{ config('links.facebook_page') }}">
-                        <x-icon class="hover:text-primary w-[12px]" icon="facebook"></x-icon>
+                        <x-icon
+                            class="hover:text-primary w-[12px]"
+                            icon="facebook"
+                        ></x-icon>
                     </a>
                     <a href="{{ config('links.instagram_page') }}">
-                        <x-icon class="hover:text-primary w-[12px]" icon="instagram"></x-icon>
+                        <x-icon
+                            class="hover:text-primary w-[12px]"
+                            icon="instagram"
+                        ></x-icon>
                     </a>
                 </div>
             </div>
